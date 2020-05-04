5e1deee38ea63eb2bacfdc903e2d537acf2f05e1
Kevin Schanz <kschanz@imobile3.com>
2020-05-04 19:30:34 -0400

    Module 3 solution - build the Parser class

--- ssg.py
+++ ssg.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
 
 import typer
+import ssg.parsers
 from ssg.site import Site
 
 
 def main(source="content", dest="dist"):
-    config: dict = {"source": source, "dest": dest}
+    config: dict = {
+        "source": source,
+        "dest": dest,
+        "parsers": [ssg.parsers.ResourceParser(), ],
+    }
     # Unpack the dictionary values
     Site(**config).build()
 
--- ssg/site.py
+++ ssg/site.py
@@ -6,16 +6,30 @@ from pathlib import Path
 class Site:
     source = None
     dest = None
+    parsers = None
 
-    def __init__(self, source, dest):
+    def __init__(self, source, dest, parsers=None):
         self.source = Path(source)
         self.dest = Path(dest)
+        self.parsers = parsers or []
 
     def create_dir(self, path):
         # Find root directory.
         directory = self.dest / path.relative_to(self.source)
         # Replace directory if it exists.
         directory.mkdir(parents=True, exist_ok=True)
+
+    def load_parser(self, extension):
+        for parser in self.parsers:
+            if parser.valid_extension(extension):
+                return parser
+
+    def run_parser(self, path):
+        parser = self.load_parser(path.suffix)
+        if parser is not None:
+            parser.parse(path, self.source, self.dest)
+        else:
+            print("Not Implemented")
 
     def build(self):
         # Make the destination directory.
@@ -24,3 +38,5 @@ class Site:
         for path in self.source.rglob(pattern="*"):
             if path.is_dir():
                 self.create_dir(path=path)
+            elif path.is_file():
+                self.run_parser(path)
